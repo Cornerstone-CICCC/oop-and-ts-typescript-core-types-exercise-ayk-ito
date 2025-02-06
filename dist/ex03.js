0baa18abe43212ce76d786b35c0bc1398f12f2b4
Ayaka Ito <a.8.32.noko.info@gmail.com>
2025-02-05 20:14:06 -0800

    done exercise

--- dist/ex03.js
+++ dist/ex03.js
@@ -1,16 +1,16 @@
+"use strict";
 // Exercise: Write a function `logValues` that takes an array of any type of values (numbers, strings, booleans, etc.).
 // The function should iterate through the array and log each value along with its type.
-
-function logValues(values: any[]) {
-  values.forEach((value) => {
-    if (typeof value === "object") {
-      console.log(`${JSON.stringify(value)} (${typeof value})`);
-    } else {
-      console.log(`${value} (${typeof value})`);
-    }
-  });
+function logValues(values) {
+    values.forEach((value) => {
+        if (typeof value === "object") {
+            console.log(`${JSON.stringify(value)} (${typeof value})`);
+        }
+        else {
+            console.log(`${value} (${typeof value})`);
+        }
+    });
 }
-
 logValues([42, "Hello", true, { name: "Alice" }]);
 // Expected output:
 // 42 (number)
